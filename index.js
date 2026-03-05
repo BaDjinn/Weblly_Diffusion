180c3120d8604de0072ec8dc2f704a3213b3db00
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2026-03-05 11:41:05 +0000

    Aggiungi la funzione di registrazione e la configurazione iniziale nel file index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import ort from 'onnxruntime-web/webgpu';
 
+function log(i) {console.log(i); document.getElementById('status').innerText += `\n${i}`;}
+
+/* Otteniamo la configurazione dall'URL*/
+
+function getConfig() {
+    const query = window.location.search.substring(1);
+    const config = {
+        model: "https://huggingface.co/schmuell/sd-turbo-ort-web/resolve/main",
+        provider: "webgpu",
+        device: "gpu",
+        threads: "1",
+        images: "2",
+    };
+    let vars = query.split("&");
+    
+}
